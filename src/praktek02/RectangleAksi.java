10828f6bf13da2202689e74acd4312d55dd890d1
panduwo <hudhapandu@gmail.com>
2023-05-28 04:33:40 +0800

    Mengisi Nilai Attribute

--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -2,6 +2,9 @@ package praktek02;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
+        
+        r1.Panjang = 9;
+        r1.Lebar = 3;
        
         
     }
